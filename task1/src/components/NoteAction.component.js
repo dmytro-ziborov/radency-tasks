911eb7b8e8c03622c860d49b7ab908141fbd9697
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-07-27 20:35:07 +0300

    fix import

--- task1/src/components/NoteAction.component.js
+++ task1/src/components/NoteAction.component.js
@@ -1,3 +1,5 @@
+import NoteService from "../services/NoteService.js";
+
 const activeActionTemplateName = "note-active-actions";
 const copyTemplate = (templateName) => {
     const template = document.querySelector(`#${templateName}`)
@@ -10,8 +12,8 @@ export const NoteAction = {
         actions.querySelector("#edit");
         actions.querySelector("#edit").setAttribute("action", "edit")
         actions.querySelector("#edit").id = note.id;
-        actions.querySelector("#archive").addEventListener("click", () => { NoteService.changeNoteStatus(note.id); Render.updateTables() })
-        actions.querySelector("#delete").addEventListener("click", () => { NoteService.deleteNoteById(note.id); Render.updateTables() })
+        actions.querySelector("#archive").addEventListener("click", () => { NoteService.changeNoteStatus(note.id); })
+        actions.querySelector("#delete").addEventListener("click", () => { NoteService.deleteNoteById(note.id); })
         return actions
     }
 }
