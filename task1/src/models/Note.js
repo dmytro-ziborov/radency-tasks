9c3a8c4384dd2808299f37ea55c60ca34baa3c8e
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-07-28 12:30:30 +0300

    moved parseDate, add setDataMethod, updateStatus

--- task1/src/models/Note.js
+++ task1/src/models/Note.js
@@ -1,14 +1,27 @@
 let idCounter = 1;
 
+//parses dates in format m/d/YYYY from content in array
+const parseDates = (value) => {
+    return value.match(/(1[0-2]|0?[1-9])\/(3[01]|[12][0-9]|0?[1-9])\/(?:[0-9]{4})/gm);
+}
+
 //Describes Note object
 export default class Note {
-    constructor(name, category, content, dates, isActive = true) {
+    constructor(name, category, content, isActive = true) {
         this.id = idCounter++;
-        this.name = name;
         this.createdAt = new Intl.DateTimeFormat("en-US", { day: "2-digit", month: "short", year: "numeric" }).format(new Date())
-        this.category = category;
-        this.content = content;
-        this.dates = dates;
+        this.setData(name, category, content);
         this.isActive = isActive;
     }
+
+    setData(name, category, content) {
+        this.name = name;
+        this.category = category;
+        this.content = content;
+        this.dates = parseDates(content);
+    }
+    updateStatus() {
+        this.isActive = !this.isActive;
+    }
+
 }
